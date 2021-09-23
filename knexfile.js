e9973db6db8dc093a127a0a7d9128a03db786e8f
Rika Ciminieri <rikaciminieri@gmail.com>
2021-09-22 19:45:22 -0700

    File setup complete

---
+++ knexfile.js
@@ -0,0 +1,16 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: "sqlite3",
+    connection: {
+      filename: "./recipe-book.db3",
+    },
+    useNullAsDefault: true,
+  },
+  pool: {
+    afterCreate: (conn, done) => {
+      conn.run("PRAGMA foreign_keys = ON", done); 
+    },
+  },
+};
